81ca472fb4c05291f9e0015f9300657982efe037
Katie Stick <katiestick@Katies-MacBook-Air.local>
2024-08-16 16:09:42 -0700

    Adding in card comp and basic styles

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -3,14 +3,15 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import axios from 'axios';
 import Button from './Components/Button';
+import Card from './Components/Card';
 function App() {
-  const [query, setQuery] = useState('');
+  const [query, setQuery] = useState([]);
   function callPokeapi(){
     axios.get('https://pokeapi.co/api/v2/pokemon/?limit=40')
       .then(function (response) {
         // handle success
-        console.log(JSON.stringify(response.data.results)); 
-        setQuery(JSON.stringify(response.data.results))
+        console.log(response.data.results); 
+        setQuery(response.data.results);
       })
       .catch(function (error) {
         // handle error
@@ -25,7 +26,14 @@ function App() {
       <header className="App-header">
         <img src={"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-iii/firered-leafgreen/16.png"} className="App-logo" alt="logo" />
         <Button onClick={callPokeapi} text={"Get pokemon"}/>
-        <p>{query}</p>
+        <div>
+        {query.map((pokemon, index) => {
+                        let name = pokemon.name;
+                        console.log('pokemon', pokemon)
+                        return <Card key={index} name={name} url={pokemon.url} />
+            })}
+            </div>
+       
       </header>
     </div>
   );
---
+++ pokedex/src/Components/Card.js
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import './Card.css';
+
+const Card=(props)=> {
+    const [img, setImg] = useState('');
+    const [backimg, setBackimg] = useState('');
+    const [weight, setWeight] = useState('');
+    const [height, setHeight] = useState('');
+    const [stats, setStats] = useState([]);
+    const [types, setTypes] = useState([]);
+    const [errorcheck, setErrorCheck] = useState(false);
+    let typesList = [];
+
+    useEffect(() => {
+        axios.get(props.url)
+            .then((res) => {
+                console.log('res', res)
+                setImg(res.data.sprites.front_default);
+                setBackimg(res.data.sprites.back_default);
+                setWeight(res.data.weight);
+                setHeight(res.data.height);
+                setStats(res.data.stats);
+                const TypesArray = res.data.types;
+                TypesArray.map((type) => {
+                    typesList.push(type.type.name);
+                    return type;
+                });
+                setTypes(typesList);
+            })
+            .catch((error) => {
+                console.log(error);
+                setErrorCheck(true);
+            })
+    }, [props.url]);
+    return(
+        <div className='card-container'>
+         <p className='card-item' key={props.index}>{props.name}-<img src={img}/></p>
+         </div>
+    );
+}
+
+export default Card;
