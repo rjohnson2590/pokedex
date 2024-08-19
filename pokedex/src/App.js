8a8c729dc1d824f3312683cc6e3c8bafef973542
Katie Stick <katiestick@Katies-MacBook-Air.local>
2024-08-19 12:54:16 -0700

    First run at pagination

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -6,8 +6,33 @@ import Button from './Components/Button';
 import Card from './Components/Card';
 function App() {
   const [query, setQuery] = useState([]);
-  function callPokeapi(){
-    axios.get('https://pokeapi.co/api/v2/pokemon/?limit=40')
+  const [currentNum, setCurrentNum]=useState(0)
+  const [currnet, setCurrnet] = useState('https://pokeapi.co/api/v2/pokemon/?limit=40&offset=20');
+  const [next, setNext]= useState('');
+  const [prev, setPrev]= useState('');
+  const [offset, setOffset]= useState(0)
+  function callPokeapi(val){ 
+    console.log(val)
+    console.log('next',next);
+    axios.get(val)
+      .then(function (response) {
+        // handle success
+        console.log(response.data); 
+       
+        setQuery(response.data.results);
+        setNext(response.data.next);
+        setPrev(response.data.previous);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      .finally(function () {
+        // always executed
+      });
+  }
+  function callPokeapiPage(){ 
+    axios.get('https://pokeapi.co/api/v2/pokemon/?limit=40&offset=')
       .then(function (response) {
         // handle success
         console.log(response.data.results); 
@@ -25,7 +50,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-iii/firered-leafgreen/16.png"} className="App-logo" alt="logo" />
-        <Button onClick={callPokeapi} text={"Get pokemon"}/>
+        <Button onClick={()=>callPokeapi(currnet)} text={"Get pokemon"}/>
         <div>
         {query.map((pokemon, index) => {
                         let name = pokemon.name;
@@ -33,7 +58,10 @@ function App() {
                         return <Card key={index} name={name} url={pokemon.url} />
             })}
             </div>
-       
+       <div>
+       <Button onClick={()=>callPokeapi(prev)} text={"Previos"}/>
+       <Button onClick={()=>callPokeapi(next)} text={"Next"}/>
+       </div>
       </header>
     </div>
   );
